f038ce458018bc76ca85886ce03e84f9183f64ec
sudarkin.ms <mikhail@sudarkin.com>
2025-03-26 10:24:58 +0500

    feat: Изменение типов зарплаты на Decimal для точности

--- src/storage/sqlalchemy/tables/jobs.py
+++ src/storage/sqlalchemy/tables/jobs.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
+from decimal import Decimal
 
-from sqlalchemy import DateTime, ForeignKey
+from sqlalchemy import DateTime, ForeignKey, Numeric
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from storage.sqlalchemy.client import Base
@@ -15,8 +16,8 @@ class Job(Base):
     )
     title: Mapped[str] = mapped_column(comment="Название вакансии")
     description: Mapped[str] = mapped_column(comment="Описание вакансии")
-    salary_from: Mapped[int] = mapped_column(comment="Зарплата от")
-    salary_to: Mapped[int] = mapped_column(comment="Зарплата до")
+    salary_from: Mapped[Decimal] = mapped_column(Numeric(10, 2), comment="Зарплата от")
+    salary_to: Mapped[Decimal] = mapped_column(Numeric(10, 2), comment="Зарплата до")
     is_active: Mapped[bool] = mapped_column(comment="Активна ли вакансия")
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
