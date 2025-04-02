8ed801cffb124307847de6ee7d95deea61e52b3f
Mikhail Sudarkin <mikhail@sudarkin.com>
2025-04-02 19:38:23 +0500

    feat: Добавлен класс UserService для управления пользователями и их репозиториями

--- src/dependencies/containers.py
+++ src/dependencies/containers.py
@@ -2,6 +2,7 @@ from dependency_injector import containers, providers
 
 from interfaces.i_sqlalchemy import ISQLAlchemy
 from repositories import UserRepository
+from services import UserService
 
 
 class RepositoriesContainer(containers.DeclarativeContainer):
@@ -13,3 +14,5 @@ class RepositoriesContainer(containers.DeclarativeContainer):
         UserRepository,
         session=db.provided.get_db,
     )
+
+    user_service = providers.Factory(UserService, user_repository=user_repository)
---
+++ src/services/user.py
@@ -0,0 +1,37 @@
+from repositories import UserRepository
+from repositories.exceptions import EntityNotFoundError, UniqueError
+from services.exception import UserAlreadyExistsError, UserNotFoundError
+from tools.security import hash_password
+from web.schemas import UserCreateSchema
+from web.schemas.user import UserUpdateSchema
+
+
+class UserService:
+    def __init__(self, user_repository: UserRepository):
+        self.user_repository = user_repository
+
+    async def create(self, user_create_dto: UserCreateSchema):
+        try:
+            return await self.user_repository.create(
+                user_create_dto, hashed_password=hash_password(user_create_dto.password)
+            )
+        except UniqueError as e:
+            raise UserAlreadyExistsError(e)
+
+    async def retrieve(self, **kwargs):
+        return await self.user_repository.retrieve(**kwargs)
+
+    async def retrieve_many(self, limit: int, skip: int):
+        return await self.user_repository.retrieve_many(limit=limit, skip=skip)
+
+    async def update(self, id: int, user_update_dto: UserUpdateSchema):
+        try:
+            return await self.user_repository.update(id=id, user_update_dto=user_update_dto)
+        except EntityNotFoundError as e:
+            raise UserNotFoundError(e)
+
+    async def delete(self, id: int):
+        try:
+            return await self.user_repository.delete(id=id)
+        except EntityNotFoundError as e:
+            raise UserNotFoundError(e)
